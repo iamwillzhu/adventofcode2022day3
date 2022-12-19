2f25099f3ad9fece59614cce7091cab0a956dd2b
William Zhu <19693587+iamwillzhu@users.noreply.github.com>
2022-12-19 01:16:40 +0000

    feat: advent of code 2022 day 3 part 2

--- main.go
+++ main.go
@@ -11,6 +11,7 @@ import (
 type Compartment string
 
 type RuckSack struct {
+	Items             string
 	FirstCompartment  Compartment
 	SecondCompartment Compartment
 }
@@ -30,6 +31,7 @@ func getRuckSackList(reader io.Reader) (ruckSackList []*RuckSack) {
 		secondCompartment := ruckSackItems[midIndex:]
 
 		ruckSack := &RuckSack{
+			Items:             ruckSackItems,
 			FirstCompartment:  Compartment(firstCompartment),
 			SecondCompartment: Compartment(secondCompartment),
 		}
@@ -43,20 +45,49 @@ func getRuckSackList(reader io.Reader) (ruckSackList []*RuckSack) {
 const ItemTypePriorityOrder = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ"
 
 func getItemTypeInBothCompartments(firstCompartment Compartment, secondCompartment Compartment) string {
-	firstCompartmentCharacterMap := make(map[string]bool)
-	secondCompartmentCharacterMap := make(map[string]bool)
+	firstCompartmentItemTypeMap := make(map[string]bool)
+	secondCompartmentItemTypeMap := make(map[string]bool)
 
-	for _, character := range firstCompartment {
-		firstCompartmentCharacterMap[string(character)] = true
+	for _, itemType := range firstCompartment {
+		firstCompartmentItemTypeMap[string(itemType)] = true
 	}
 
-	for _, character := range secondCompartment {
-		secondCompartmentCharacterMap[string(character)] = true
+	for _, itemType := range secondCompartment {
+		secondCompartmentItemTypeMap[string(itemType)] = true
 	}
 
-	for character, _ := range firstCompartmentCharacterMap {
-		if _, ok := secondCompartmentCharacterMap[character]; ok {
-			return character
+	for itemType, _ := range firstCompartmentItemTypeMap {
+		if _, ok := secondCompartmentItemTypeMap[itemType]; ok {
+			return itemType
+		}
+	}
+
+	return ""
+}
+
+func getItemTypeInRuckSacks(ruckSackOne *RuckSack, ruckSackTwo *RuckSack, ruckSackThree *RuckSack) string {
+	ruckSackOneItemTypeMap := make(map[string]bool)
+	ruckSackTwoItemTypeMap := make(map[string]bool)
+	ruckSackThreeItemTypeMap := make(map[string]bool)
+
+	for _, itemType := range ruckSackOne.Items {
+		ruckSackOneItemTypeMap[string(itemType)] = true
+	}
+
+	for _, itemType := range ruckSackTwo.Items {
+		ruckSackTwoItemTypeMap[string(itemType)] = true
+	}
+
+	for _, itemType := range ruckSackThree.Items {
+		ruckSackThreeItemTypeMap[string(itemType)] = true
+	}
+
+	for itemType, _ := range ruckSackOneItemTypeMap {
+		_, inRuckSackTwo := ruckSackTwoItemTypeMap[itemType]
+		_, inRuckSackThree := ruckSackThreeItemTypeMap[itemType]
+
+		if inRuckSackTwo && inRuckSackThree {
+			return itemType
 		}
 	}
 
@@ -82,18 +113,26 @@ func main() {
 
 	ruckSackList := getRuckSackList(file)
 	sumOfItemTypeInBothCompartmentsPriority := 0
+	sumOfItemTypeInRuckSacksPriority := 0
 
 	for index, ruckSack := range ruckSackList {
 		itemTypeInBothCompartments := getItemTypeInBothCompartments(ruckSack.FirstCompartment, ruckSack.SecondCompartment)
 
 		if itemTypeInBothCompartments == "" {
-			log.Fatalf("No item type in both compartments of rucksack %d", index)
-			continue
+			log.Panicf("No item type in both compartments of rucksack %d", index)
 		}
 
 		sumOfItemTypeInBothCompartmentsPriority += getItemTypePriority(itemTypeInBothCompartments)
-		fmt.Printf("rucksack :%d, first compartment: %s, second compartment: %s, item type in both compartments: %s\n", index, ruckSack.FirstCompartment, ruckSack.SecondCompartment, itemTypeInBothCompartments)
+		fmt.Printf("rucksack :%d, first compartment: %s, second compartment: %s, item type in both compartments: %s\n", index+1, ruckSack.FirstCompartment, ruckSack.SecondCompartment, itemTypeInBothCompartments)
+
+		if index%3 == 2 && index >= 2 {
+			itemTypeInRuckSacks := getItemTypeInRuckSacks(ruckSackList[index-2], ruckSackList[index-1], ruckSackList[index])
+			fmt.Printf("rucksacks %d to %d badge item type is %s\n\n", index-1, index+1, itemTypeInRuckSacks)
+
+			sumOfItemTypeInRuckSacksPriority += getItemTypePriority(itemTypeInRuckSacks)
+		}
 	}
 
-	fmt.Printf("The sum of the priorities of the above item types is %d\n", sumOfItemTypeInBothCompartmentsPriority)
+	fmt.Printf("The sum of the priorities of the item types in both compartments for all rucksacks is %d\n", sumOfItemTypeInBothCompartmentsPriority)
+	fmt.Printf("The sum of the priorities of the item types that are badges for rucksacks in groups of three is %d\n", sumOfItemTypeInRuckSacksPriority)
 }
